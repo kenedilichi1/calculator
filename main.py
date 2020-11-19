3ab136426a527bb439575064d360f9f24fb62261
kenedilichi1 <henryobeta@gmail.com>
2020-11-19 18:45:03 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,44 @@
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
+isRunning = True
+
+while isRunning:
+    choice = input("operator: ")
+
+    if choice in ('add', 'subtract', 'multiply', 'divide'):
+        num1 = float(input("Enter num1: "))
+        num2 = float(input("Enter num2: "))
+
+        if choice == 'add':
+            result = add(num1, num2)
+            
+            print(int(result))
+        
+        elif choice == 'subtract':
+            result = subtract(num1, num2)
+            print(int(result))
+        
+        elif choice == 'multiply':
+            result = multiply(num1, num2)
+            
+            print(int(result))
+        
+        elif choice == 'divide':
+            result = divide(num1, num2)
+            
+            print(int(result))
+    else:
+        print("Input error")
+    
